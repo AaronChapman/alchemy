bf86ef6c0d8cb995a525762b683e6989161d6685
Aaron Chapman <aaronchapman95@gmail.com>
2018-09-30 18:12:16 -0400

    working through data input

--- javascript/alchemy.js
+++ javascript/alchemy.js
@@ -20,13 +20,13 @@ var ingredient_matches = [{
 	}
 ];
 
-function check_active_ingredients_for_matches (first_ingredient, second_ingredient) {
+function check_active_ingredients_for_matches(first_ingredient, second_ingredient) {
 	for (var i = 0; i < ingredient_matches.length; i++) {
 		if (ingredient_matches[i].name === first_ingredient) {
 			for (var j = 0; j < ingredient_matches[i].matches.length; j++) {
 				if (ingredient_matches[i].matches[j].ingredient === second_ingredient) {
 					var ingredient_produced = ingredient_matches[i].matches[j].product;
-					
+
 					available_ingredients.push(ingredient_produced);
 
 					$('.produced-ingredients').append('<div class="ingredient" data-ingredient="' + ingredient_produced + '"><img src="images/' + ingredient_produced + '.png"><label class="ingredient-label">' + ingredient_produced + '</label></div>');
@@ -36,14 +36,38 @@ function check_active_ingredients_for_matches (first_ingredient, second_ingredie
 	}
 }
 
-function fill_available_ingredients_container () {
+function fill_available_ingredients_container() {
 	available_ingredients.forEach(function (item) {
 		$('.available-ingredients').append('<div class="ingredient" data-ingredient="' + item + '"><img src="images/' + item + '.png"><label class="ingredient-label">' + item + '</label></div>');
 	});
 }
 
+function fill_ingredient_matches_array() {
+	var ingredients = ingredient_list.split('! [ingredient]:');
+	
+	for (var i = 0; i < ingredients.length; i++) {
+		var ingredient = ingredients[i].substring(0, ingredients[i].indexOf('~')).trim();
+		var match_ingredients = ingredients[i].substring(ingredients[i].indexOf('~'),  ingredients[i].length).split('~');
+		var matches = [];
+		
+		for (var j = 0; j < match_ingredients.length; j++) {
+			var match = match_ingredients[j].substring(4 + ingredient.length, match_ingredients[j].indexOf('=')).trim();
+			var product =  match_ingredients[j].substring(match_ingredients[j].indexOf('='), match_ingredients[j].length);
+
+			matches.push({'ingredient':match, 'product':product});
+		}
+		
+		ingredient_matches.push({'name':ingredient, 'matches':matches});
+		
+		//matches.push({'ingredient':ingredients[i].substring(ingredients[i].indexOf('+'), ingredients[i].indexOf('=')).trim(), 'product':ingredients[i].indexOf('=')) });
+	}
+	
+	console.log(ingredient_matches);
+}
+
 $(document).ready(function () {
 	fill_available_ingredients_container();
+	fill_ingredient_matches_array();
 
 	$('.available-ingredients .ingredient').click(function () {
 		if ($('.active-ingredients .ingredient').length < 2) {
